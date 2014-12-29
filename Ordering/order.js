a50ab5f8a16d0d6597962526de05b5811598e5b3
bgyhnddr <bgyhnddr@163.com>
2014-12-29 21:53:24 +0800

    123123

---
+++ Ordering/order.js
@@ -0,0 +1,23 @@
+﻿
+//  $.feat.nativeTouchScroll=true;
+$(document).ready(function () {
+    $.ui.launch();
+
+    order.bindEvents();
+    
+
+});
+
+var order = {
+    load: {
+        page_detail_load: function () {
+            $.ui.hide
+        }
+    },
+    currentId: "",
+    bindEvents: function () {
+        addOrder.addEventListener("touchstart", function (ev) {
+            $.ui.loadContent("page_detail_header");
+        });
+    }
+}
